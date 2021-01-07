e285f2cc6cec821b4b7ef88bc49c9fbb28d084b2
Rafet Topçu <rafet1998@gmail.com>
2021-01-07 16:48:10 +0300

    morphology written globally

--- app.py
+++ app.py
@@ -7,6 +7,13 @@ from flask_cors import CORS
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
+
+
+ZEMBEREK_PATH = r'data/zemberek-full.jar'
+startJVM(getDefaultJVMPath(), '-ea', '-Djava.class.path=%s' % (ZEMBEREK_PATH))
+TurkishMorphology = JClass('zemberek.morphology.TurkishMorphology')
+morphology = TurkishMorphology.createWithDefaults()
+
 @app.route('/')
 def hello_world():
     try:
@@ -20,15 +27,11 @@ def hello_world():
             'result': result,
             'track' : track
         }
-    except:
+    except Exception as e:
+        print(e)
         return {
             'message': 'error'
         },500
-morphology = None
+
 if __name__ == "__main__":
-    if not morphology:
-        ZEMBEREK_PATH = r'data/zemberek-full.jar'
-        startJVM(getDefaultJVMPath(), '-ea', '-Djava.class.path=%s' % (ZEMBEREK_PATH))
-        TurkishMorphology = JClass('zemberek.morphology.TurkishMorphology')
-        morphology = TurkishMorphology.createWithDefaults()
     app.run(debug=True, port=5000)
